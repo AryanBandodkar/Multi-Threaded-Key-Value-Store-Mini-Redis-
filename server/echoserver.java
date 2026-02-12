4fcdfa7b29e0f7c10b6b3487d7024dffbd1dd2c2
AryanBandodkar <aryanbandodkar5@gmail.com>
2026-02-12 22:04:38 +0530

    Added threads per client

--- server/echoserver.java
+++ server/echoserver.java
@@ -2,110 +2,113 @@ package server;
 import java.io.*;
 import java.net.*;
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class echoserver {
-    public static void main(String[] args) {
+    private static ConcurrentHashMap<String, String> store = new ConcurrentHashMap<>();
 
-        HashMap<String, String> store = new HashMap<>();
+    public static void main(String[] args) throws IOException {
 
-        try {
-            
-            //Server Connection to port 8080
-            ServerSocket serverSocket = new ServerSocket(8080);
+        try(ServerSocket serverSocket = new ServerSocket(8080);){
+
+           
             System.out.println("Server Started (Using port 8080 )");
 
-            //Waiting (Blocked) till client connects to the server
-            Socket clientSocket = serverSocket.accept();
-            System.out.println("Client Connected to the Server");
+            //Having a thread for each client that connects
+            while (true) {
 
-            // I/o readers for taking the client message and sending it back
-            BufferedReader input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            PrintWriter output = new PrintWriter(clientSocket.getOutputStream(), true);
-
-            String message;
-
-            //loop till client closes
-            while ((message = input.readLine()) != null) {
-                
-                //checks if the message is empty or just spaces are sent
-                if (message.trim().isEmpty()) {
-                    output.println("Empty command given");
-                    continue;
-                }
-
-                //Assume that spaces are there between each word/command 
-                String trimmed = message.trim();
-                String[] parts = trimmed.split("\\s+",3);
-                //Gets the command and makes it to upper case for easier processing
-                String command = parts[0].toUpperCase();
-
-                switch (command) {
-                    case "SET":
-                        //checks if the set command isnt used properly
-                        if (parts.length != 3) {
-                            output.println("Incomplete command. Format Set <key> <value>");
-                        }
-                        else {
-
-                            //extracts the key value pair nd puts it in the hashmap
-                            String key = parts[1];
-                            String value = parts[2];
-                            store.put(key, value);
-                            output.println("Data Stored Successfully");
-                        }
-
-                        break;
-
-                    case "GET":
-                        
-                        if (parts.length != 2) {
-                            output.println("Incomplete Command . Format Get <key>");
-
-                        }
-                        else {
-                            String key = parts[1];
-                            if (store.containsKey(key)) {
-                                output.println("Value is : " + store.get(key));
-                            }
-                            else {
-                                output.println("Key Doesnt Exist");
-                            }
-                        }
-
-                        break;
-
-                    case "DEL":
-
-                        if (parts.length != 2){
-                            output.println("Incomplete Command. Format Del <key>");
-                        }
-                        else {
-                            String key = parts[1];
-                            if (store.containsKey(key)) {
-                                store.remove(key);
-                                output.println("Key Removed Successfully");
-                            } else {
-                                output.println("Key not Found");
-                            }
-                        }
-                        break;
-                        
-                    default:
-                        output.println("Unknown command (set,get,del)");
-
-                }
-
-
+                Socket client = serverSocket.accept();
+                new Thread(() -> handleClient(client)).start();
             }
+
             
             //closes for mem leak
-            clientSocket.close();
-            serverSocket.close();
+            
+            
 
-
-        }
-        catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
+    
+    private static void handleClient(Socket client) {
+        try {
+            //Done for each client
+            BufferedReader input = new BufferedReader(new InputStreamReader(client.getInputStream()));
+            PrintWriter output = new PrintWriter(client.getOutputStream(), true);
+
+            String message;
+
+            while ((message = input.readLine()) != null) {
+                String response = handleCommand(message);
+                output.println(response);
+            }
+            client.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+    }
+    
+    private static String handleCommand(String message) {
+        
+        //Assume that spaces are there between each word/command 
+        String trimmed = message.trim();
+        String[] parts = trimmed.split("\\s+", 3);
+        //Gets the command and makes it to upper case for easier processing
+        String command = parts[0].toUpperCase();
+        
+        switch (command) {
+            case "SET":
+                //checks if the set command isnt used properly
+                if (parts.length != 3) {
+                    return "Incomplete command. Format Set <key> <value>";
+                } else {
+        
+                    //extracts the key value pair nd puts it in the hashmap
+                    String key = parts[1];
+                    String value = parts[2];
+                    store.put(key, value);
+                   return "Data Stored Successfully";
+                }
+        
+                
+        
+            case "GET":
+        
+                if (parts.length != 2) {
+                    return "Incomplete Command . Format Get <key>";
+        
+                } else {
+                    String key = parts[1];
+                    if (store.containsKey(key)) {
+                        return store.get(key);
+                    } else {
+                        return "Key Doesnt Exist";
+                    }
+                }
+        
+                
+        
+            case "DEL":
+        
+                if (parts.length != 2) {
+                    return "Incomplete Command. Format Del <key>";
+                } else {
+                    String key = parts[1];
+                    if (store.containsKey(key)) {
+                        store.remove(key);
+                        return "Key Removed Successfully";
+                    } else {
+                        return "Key not Found";
+                    }
+                }
+                
+        
+            default:
+                return "Unknown command (set,get,del)";
+        
+        }
+    }
+
 }
